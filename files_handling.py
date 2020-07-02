38a1455aa1e29cec08723167cb359b9e84e4feff
Damian Cholewa <cholewa.damian1997@gmail.com>
2020-07-02 12:24:18 +0200

    Update files_handling.py

--- files_handling.py
+++ files_handling.py
@@ -4,11 +4,11 @@ import os
 CURRENT_DIRECTORY = os.getcwd()
 
 # Files names:
-AI_PATENTS = "CURRENT_DIRECTORY/Data/artificial_inteligence_patents_aplications.xlsx"
-LIDAR_PATENTS = "CURRENT_DIRECTORY/Data/owners_of_LIDAR_patents.xlsx"
-AUTONOMOUS_DRIVING_PATENT = "CURRENT_DIRECTORY/Data/autonomous_driving_patent_owners.xlsx"
-VISION_MARKET_SIZE_CHINA = "CURRENT_DIRECTORY/Data/vision_industry_market_size_in_china.xlsx"
-VISION_MARKET_SIZE_USA = "CURRENT_DIRECTORY/Data/machine_vision_market_usa.xlsx"
-VISION_MARKET_SIZE_IN_MEDICINE = "CURRENT_DIRECTORY/Data/computer_vision_ai_market_revenue_medicine.xlsx"
-FOUNDING_AI_STARTUPS_ISRAEL = "CURRENT_DIRECTORY/Data/funding_of_ai-startups_israel.xlsx"
-FOUNDING_AI_STARTUPS_WORLD = "CURRENT_DIRECTORY/Data/funding_of_ai-startups_worldwide.xlsx"
+AI_PATENTS = CURRENT_DIRECTORY+"/Data/artificial_inteligence_patents_aplications.xlsx"
+LIDAR_PATENTS = CURRENT_DIRECTORY+"/Data/owners_of_LIDAR_patents.xlsx"
+AUTONOMOUS_DRIVING_PATENT = CURRENT_DIRECTORY+"/Data/autonomous_driving_patent_owners.xlsx"
+VISION_MARKET_SIZE_CHINA = CURRENT_DIRECTORY+"/Data/vision_industry_market_size_in_china.xlsx"
+VISION_MARKET_SIZE_USA = CURRENT_DIRECTORY+"/Data/machine_vision_market_usa.xlsx"
+VISION_MARKET_SIZE_IN_MEDICINE = CURRENT_DIRECTORY+"/Data/computer_vision_ai_market_revenue_medicine.xlsx"
+FOUNDING_AI_STARTUPS_ISRAEL = CURRENT_DIRECTORY+"/Data/funding_of_ai-startups_israel.xlsx"
+FOUNDING_AI_STARTUPS_WORLD = CURRENT_DIRECTORY+"/Data/funding_of_ai-startups_worldwide.xlsx"
